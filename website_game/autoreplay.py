ddf8128b7f6de5a36d2ebdc8e928534402525441
munyuawilson <wmunyua4@gmail.com>
2023-06-20 20:40:09 +0300

    update

--- website_game/autoreplay.py
+++ website_game/autoreplay.py
@@ -20,52 +20,51 @@ def choose_chapter():
     y=460
     
     pyautogui.click(x=x,y=y)
-    print(pyautogui.position())
+    
 
 def start_chapter_one():
     x=387
     y=299
     pyautogui.click(x=x,y=y)
-    
+def position():
+    time.sleep(5)
+    print(pyautogui.position())
+
+   
 def detect_object():
-    screenshot = ImageGrab.grab()
-
-    # Convert the screenshot to OpenCV format
-    screenshot_cv = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-
-    # Preprocessing (example: converting to grayscale)
-    gray_screenshot = cv2.cvtColor(screenshot_cv, cv2.COLOR_BGR2GRAY)
-
-    # Object Detection (example: template matching)
-    template = cv2.imread('black_guy.png', 0)  # Replace with the path to your template image
-    result = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8  # Adjust the threshold as per your requirement
-    locations = np.where(result >= threshold)
-    object_coordinates = list(zip(*locations[::-1]))
-    return object_coordinates
+    if pyautogui.locateOnScreen("black_guy.png"):
+        return True
+    elif pyautogui.locateOnScreen("redguy.png"):
+        return True
+    elif pyautogui.locateOnScreen("pink_guy.png"):
+        return True
     
-def play_game():  
-    pyautogui.keyUp('right') 
+    else:
+        return False
+    
+def play_game():
+    pyautogui.keyDown('right')
+    time.sleep(2)
+    pyautogui.keyUp('right')
+    
+
+    
+    while True:
+        
+        pyautogui.keyDown('z')
+        pyautogui.keyUp('z')
+        pyautogui.keyDown('x')
+        pyautogui.keyUp('x')
+        
+        
+        
+
+    pyautogui.keyDown('right')
     time.sleep(3)
-    pyautogui.keyDown('right') 
-    object_coordinates=detect_object()
-    print(object_coordinates)
-    
-
-    
-    if object_coordinates:
-        keyboard.press("x")
-        time.sleep(1)
-        keyboard.release("x")
-        keyboard.press("y")
-        time.sleep(1)
-        keyboard.release("y")
+    pyautogui.keyUp('right') 
         
     
     
-    
-    
-
 
 
         
@@ -78,5 +77,7 @@ time.sleep(5)
 start_chapter_one()
 time.sleep(5)
 play_game()
+
+
     
     
